3c3eef1d77149945628b5072195be226cc9e9c8e
reiok0208 <rei.okada.wii@gmail.com>
2020-04-30 14:11:53 +0000

    Add a member devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.creart!(
+Admin.create!(
   name:"管理者１",
   email:"admin@mail.com",
   password:"admin1",
