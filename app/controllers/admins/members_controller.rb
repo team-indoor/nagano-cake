cb5da0700ebc1bef11a9411982dce3144e2550cc
takaakikobayashi <takakoba0426@gmail.com>
2020-05-01 09:25:38 +0000

    add

--- app/controllers/admins/members_controller.rb
+++ app/controllers/admins/members_controller.rb
@@ -1,10 +1,13 @@
 class Admins::MembersController < ApplicationController
   def index
+  	@members = Member.all
   end
 
   def show
+  	@members = Member.all
   end
 
   def edit
+  	@members = Member.all
   end
 end
