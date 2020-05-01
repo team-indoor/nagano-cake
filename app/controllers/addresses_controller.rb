008e340634d104ea3170b19012c5d2b79ec9c2fa
takaakikobayashi <takakoba0426@gmail.com>
2020-05-01 13:24:47 +0000

    First commit

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,18 @@
 class AddressesController < ApplicationController
   def index
+  	@addresses = Address.all
+  	@address = Address.find(params[:id])
   end
 
   def edit
+  	@address = Address.find(params[:id])
   end
+
+  def destroy
+  	@address = 
+  end
+private
+    def address_params
+      params.require(:address).permit(:postal_code, :address, :reciever)
+    end
 end
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,10 +1,16 @@
 class MembersController < ApplicationController
   def show
+  	@members = Member.all
   end
 
   def edit
+  	@member =Member.find(params[:id])
   end
 
   def delete
   end
 end
+private
+    def member_params
+      params.require(:member).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :email, :postal_code, :address, :phone_number, :password)
+    end
