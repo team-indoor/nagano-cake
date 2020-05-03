f8f702b04f00974a2b15bef26fc2660ec20bec1c
takaakikobayashi <takakoba0426@gmail.com>
2020-05-03 06:05:34 +0000

    First commit

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,15 @@
 class AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
+    @member = Member.find(params[:member_id])
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
+
+  def destroy
+  end
+
 end
