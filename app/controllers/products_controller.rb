864c4ea8ba5c172c385073c7f08a227ee40d9dc7
reiok0208 <rei.okada.wii@gmail.com>
2020-05-03 12:18:40 +0900

    Fix a home product function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,17 @@
 class ProductsController < ApplicationController
   def index
     @categories = Category.all
-    @products = Product.all
+    if params["category_id"] == "1"
+      @products = Product.where(category_id:params["category_id"])
+    elsif params["category_id"] == "2"
+      @products = Product.where(category_id:params["category_id"])
+    elsif params["category_id"] == "3"
+      @products = Product.where(category_id:params["category_id"])
+    elsif params["category_id"] == "4"
+      @products = Product.where(category_id:params["category_id"])
+    else
+      @products = Product.all
+    end
   end
 
   def show
