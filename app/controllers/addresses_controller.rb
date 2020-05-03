ffa9144301544f299a71b28d31e265cca972576e
takaakikobayashi <takakoba0426@gmail.com>
2020-05-03 07:18:43 +0000

    modified

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -9,7 +9,36 @@ class AddressesController < ApplicationController
     @address = Address.find(params[:id])
   end
 
-  def destroy
+  def create
+    @address = Address.new(address_params)
+    @address.member_id = params[:member_id].to_i
+    if @address.save!
+       redirect_to member_addresses_path
+    else
+       @addresses = Address.all
+       @member = Member.find(params[:member_id])
+       render action: :index
+    end
   end
 
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+       redirect_to member_addresses_path
+    else
+       @address = Address.find(params[:id])
+       render action: :edit
+    end
+  end
+
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to member_addresses_path
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :reciever, :member_id)
+  end
 end
