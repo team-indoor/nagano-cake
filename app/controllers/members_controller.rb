cffd7bdd056e33ba6976538c7ea3160c7d19f4ee
kamitoshi <kroo.aq.74@gmail.com>
2020-05-04 19:45:23 +0900

    modify

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,29 +1,44 @@
 class MembersController < ApplicationController
+  before_action :authenticate_member!
+
   def show
   	@member = Member.find(params[:id])
   end
 
   def edit
-  	@member =Member.find(params[:id])
+  	@member = Member.find(params[:id])
   end
 
   def update
-    @member =Member.find(params[:id])
+    @member = Member.find(params[:id])
     if @member.update(member_params)
-       redirect_to action: :show
-  else
-    @member =Member.find(params[:id])
-    render action: :edit
+      redirect_to action: :show
+    else
+      @member = Member.find(params[:id])
+      render action: :edit
+    end
+  end
+
+  def delete
   end
 
   def destroy
     @member = Member.find(params[:id])
-    @member.destroy
+    @member.update(is_active: false)
     redirect_to root_path
   end
-end
-private
-    def member_params
-      params.require(:member).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :email, :postal_code, :address, :phone_number)
-    end
+
+  private
+  def member_params
+    params.require(:member).permit(
+      :last_name,
+      :first_name,
+      :kana_last_name,
+      :kana_first_name,
+      :email, :postal_code,
+      :address,
+      :phone_number, 
+      :is_active
+    )
+  end
 end
