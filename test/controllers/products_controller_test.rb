302ba04a574854b211030ee7c6fa59627fac099b
reiok0208 <rei.okada.wii@gmail.com>
2020-05-13 19:59:40 +0900

    Fix a cart transition & minitest

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,15 +4,22 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     @product = products(:product1)
+    @category = categories(:category)
   end
 
-  test "should get index" do
+  test "商品の一覧画面の取得（全商品）" do
     get products_path
     assert_response :success
   end
 
-  test "should get show" do
+  test "商品の詳細画面の取得" do
     get product_path(@product)
+    assert_response :success
+  end
+
+  test "商品の一覧画面の取得（カテゴリー別）" do
+    get products_path(category_id: @category.id)
+    #assert_select "div", @category.name
     assert_response :success
   end
 
